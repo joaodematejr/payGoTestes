2b655d5d09f38a9fc95d4f8fb4d3dfa40e4c9e04
joaodematejr <joaodematejr@gmail.com>
2022-02-02 16:26:47 -0300

    Versionamento do projeto

---
+++ index.js
@@ -0,0 +1,14 @@
+var ffi = require('ffi-napi');
+var ref = require('ref-napi');
+var struct = require('ref-struct-di');
+var array = require('ref-array-napi');
+var finalize = require('finalize');
+
+
+console.log("https://github.com/redredgroovy/go-ffi-demo")
+
+console.log("ffi", ffi);
+console.log("ref", ref);
+console.log("struct", struct);
+console.log("array", array);
+console.log("finalize", finalize);
